b5cb0fe345d2fc817e7895ca157035664e788485
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-09-26 18:43:31 +0300

    install Django 1.9

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Dropbox\Scoala Informala de IT\PycharmProjects\CursUdemy-1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
