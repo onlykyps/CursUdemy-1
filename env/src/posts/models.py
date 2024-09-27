25b346f0336ab543587831d7f72faf90f2ba8c0c
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-09-27 18:29:52 +0300

    first migrations

--- env/src/posts/models.py
+++ env/src/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+	title = models.CharField(max_length=20)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	update = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+	# python 2
+	def __unicode__(self):
+		return self.title
+
+	# python 3
+	def __str__(self):
+		return self.title
