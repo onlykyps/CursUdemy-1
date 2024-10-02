48e74395ec76063341ddc73d780e144b5740361c
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-10-02 06:10:21 +0300

    context

--- env/src/posts/views.py
+++ env/src/posts/views.py
@@ -10,10 +10,29 @@ def post_create(request):
 	return HttpResponse("<h1>Hello</h1>")
 
 def post_detail(request):
-	return HttpResponse("<h1>Hello</h1>")
+	context_data = {
+		"title":"detail"
+	}
+	return render(request, "index.html", context_data)
+	# return HttpResponse("<h1>Hello</h1>")
 
 def post_list(request):
-	return render(request, "index.html", {})
+	if request.user.is_authenticated():
+		{
+			context_data = {
+								"title":"my user list"
+							}		
+			return render(request, "other_page.html", context_data)
+		}
+	else
+		{
+			context_data = {
+								"title":"list"
+							}		
+		}
+
+	
+	return render(request, "index.html", context_data)
 	# return HttpResponse("<h1>Hello</h1>")
 
 def post_update(request):
